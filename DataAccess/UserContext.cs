52bd59dd03a07325b87d091d93120f44c2b1a50b
chaman.raghav <chaman.raghav@vicesoftware.com>
2020-04-15 20:23:03 +0530

    Add project files.

---
+++ DataAccess/UserContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Modals;
+
+namespace WebApplication1.DataAccess
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions options) : base(options) { }
+        public DbSet<User> Users { get; set; }
+    }
+}
