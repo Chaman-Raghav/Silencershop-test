c7e282d25acc7995b97e7084c216768f33d26026
chaman.raghav <chaman.raghav@vicesoftware.com>
2020-04-16 18:43:22 +0530

    Added enpoints for AddUser, DeleteUser & EditUser. Used datafetching from url `QueryString`. Made all DB changes in try catch blocks, and Catched the exception using `DbUpdateException`. Checked all endpoints via Postman, work fine.

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using WebApplication1.DataAccess;
+using WebApplication1.Modals;
 
 namespace WebApplication1.Controllers
 {
@@ -24,5 +26,60 @@ namespace WebApplication1.Controllers
             var users = _context.Users.ToList();
             return users;
         }
+
+        [HttpPost]
+        [Route("AddUser")]
+        public OkResult PostUser(User user)
+        {
+            try
+            {
+                _context.Users.Add(user);
+                _context.SaveChanges();
+            }
+            catch(DbUpdateException ex)
+            {
+                Console.WriteLine("There is some error in Adding the user:", ex);
+                throw;
+            }
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("deleteUser/{userId:int}")]
+        public OkResult DeleteUser(int userId)
+        {
+            try
+            {
+                var user = _context.Users.FirstOrDefault(user => (user.Id == userId));
+                if(user != null)
+                {
+                    _context.Users.Remove(user);
+                    _context.SaveChanges();
+                }
+            }
+            catch(DbUpdateException ex)
+            {
+                Console.WriteLine("Error while deleting the user:", ex.InnerException.Message);
+                throw;
+            }
+            return Ok();
+        }
+        [HttpPut]
+        [Route("EditUser")]
+        public IActionResult EditUserDetails(User user)
+        {
+            try
+            {
+                _context.Users.Update(user);
+                _context.SaveChanges();
+            }
+            catch(DbUpdateException ex)
+            {
+                Console.WriteLine("Error while Updating the user:", ex.InnerException.Message);
+                throw;
+            }
+            return Ok();
+        }
+
     }
 }
