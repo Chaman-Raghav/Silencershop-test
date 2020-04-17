69d0df465bbb48b80afceaaf2f78970cddd4f941
chaman.raghav <chaman.raghav@vicesoftware.com>
2020-04-17 19:44:11 +0530

    Created an Interface for Documents, having same attributes, but used IFormFile For image.? Added an method `postdocument`, in which we fetched the values from the postman using `FromForm` tag. Faced an Error, when saving the document `Con not convert the IFormFile to byte[]. Hence Converted the IFormFile to array of byte using `CopyAsync` method and then save the Image in byte to DB, Able to successfully saved the file to DB.

--- Controllers/DocumentsController.cs
+++ Controllers/DocumentsController.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WebApplication1.DataAccess;
+using WebApplication1.Interfaces;
 using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
@@ -14,24 +16,50 @@ namespace WebApplication1.Controllers
     [ApiController]
     public class DocumentsController : ControllerBase
     {
-        private readonly UserContext _context;
+        private readonly UserContext _context; 
         public DocumentsController(UserContext context)
         {
             _context = context;
         }
 
-        [HttpPost]
-        [Route("SaveDocument")]
-        public IActionResult SaveDocument(Document document)
+        [HttpGet]
+        [Route("GetDocument/{documentId:int}")]
+        public IDocument GetDocuments(int documentId)
         {
+            var document = _context.Documents.FirstOrDefault(document => document.Id == documentId);
+            var stream = new MemoryStream(document.Image);
+            IFormFile file = new FormFile(stream, 0, document.Image.Length, "name", "fileName");
+            IDocument returnDocument =  new IDocument
+            {
+                Name = document.Name,
+                Image = file
+            };
+            return returnDocument;
+        }
+
+        [HttpPost]
+        public async Task<OkResult> PostDocument([FromForm]IDocument document)
+        {
+            var ImageInBytes = new byte[document.Image.Length];
+            if (document.Image.Length > 0)
+            {
+                using (var stream = new MemoryStream())
+                {
+                    await document.Image.CopyToAsync(stream);
+                    ImageInBytes = stream.ToArray();
+                }
+            }
             try
             {
-                _context.Add(document);
+                _context.Documents.Add(new Document { 
+                    Name= document.Name,
+                    Image = ImageInBytes
+                });
                 _context.SaveChanges();
             }
-            catch(DbUpdateException ex)
+            catch (DbUpdateException ex)
             {
-                Console.WriteLine("Error Occured while saving the document", ex.InnerException.Message);
+                Console.WriteLine("There is some error in fetching the Document:", ex.InnerException.Message);
                 throw;
             }
             return Ok();
